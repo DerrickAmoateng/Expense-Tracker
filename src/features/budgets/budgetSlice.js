6c34b0e8e9d94e748841aa37aef60a5208a4380e
DerrickAmoateng <99464972+DerrickAmoateng@users.noreply.github.com>
2022-04-28 20:57:35 +0000

    add files to repo

---
+++ src/features/budgets/budgetSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CATEGORIES = ['housing', 'food', 'transportation', 'utilities', 'clothing', 'healthcare', 'personal', 'education', 'entertainment'];
+const initialState = CATEGORIES.map(category => ({ category: category, amount: 0 }))
+
+const budgetsSlice = createSlice({
+  name: 'budgets',
+  initialState: initialState,
+  reducers: {
+    editBudget: (state, action) => {
+       const budgetIndex = state.findIndex(budget => budget.category === action.payload.category);
+      state[budgetIndex].amount = action.payload.amount;
+    }
+  }
+})
+
+export const selectBudgets = (state) => state.budgets;
+export const {editBudget} = budgetsSlice.actions;
+export default budgetsSlice.reducer;
